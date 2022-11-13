b5ea244c72f05b1dc262af817b2adaabf64644cb
Afif Permana <afifp04@gmail.com>
2022-11-13 16:28:18 +0700

    Initial commit

---
+++ src/main/java/com/afifpermana/anggota/service/repository/AnggotaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.afifpermana.anggota.service.repository;
+
+import com.afifpermana.anggota.service.entity.Anggota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Apip
+ */
+@Repository
+public interface AnggotaRepository extends JpaRepository<Anggota, Long>{
+
+    public Anggota findByAnggotaId(Long anggotaId);
+    
+}
---
+++ src/main/java/com/afifpermana/anggota/service/service/AnggotaService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.afifpermana.anggota.service.service;
+
+import com.afifpermana.anggota.service.entity.Anggota;
+import com.afifpermana.anggota.service.repository.AnggotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Apip
+ */
+@Service
+public class AnggotaService {
+    @Autowired
+    private AnggotaRepository anggotaRepository;
+    
+    public Anggota saveAnggota(Anggota anggota){
+        return anggotaRepository.save(anggota);
+    }
+    
+    public Anggota findAnggotaById(Long anggotaId){
+        return anggotaRepository.findByAnggotaId(anggotaId);
+    }
+}
